1782f15f64790f9006737d1cfb79182499a99414
Ethan <ethan.zieba@laplateforme.io>
2024-02-19 17:23:59 +0100

    Update on server console print
    Fixed "DATA" and "HEADERS" message

--- handler.py
+++ handler.py
@@ -11,7 +11,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         data = self.rfile.read(content_length)
-        print(f"POST request:\nHeaders: {self.headers}\nDate: {data.decode('utf-8')}")
+        print(f"POST request:\nHEADERS: {self.headers}\nDATA: {data.decode('utf-8')}")
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
