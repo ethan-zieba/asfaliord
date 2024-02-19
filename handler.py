c498ce33b34debcefdfbcd628c133ed29f7c6eb3
Ethan <ethan.zieba@laplateforme.io>
2024-02-19 17:03:05 +0100

    Separated server.py into two files
    Handler class in handler.py, Server class in server.py

---
+++ handler.py
@@ -0,0 +1,17 @@
+from http.server import BaseHTTPRequestHandler
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        print("Get request")
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        data = self.rfile.read(content_length)
+        print(f"POST request:\nHeaders: {self.headers}\nDate: {data.decode('utf-8')}")
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
--- server.py
+++ server.py
@@ -1,26 +1,11 @@
-import http.server
+from http.server import HTTPServer
+from handler import Handler
 
 
-class Server(http.server.HTTPServer):
+class Server(HTTPServer):
     def __init__(self, ip, port):
         super().__init__((ip, port), Handler)
         self.serve_forever()
-
-
-class Handler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        print("Get request")
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        data = self.rfile.read(content_length)
-        print(f"POST request:\nHeaders: {self.headers}\nDate: {data.decode('utf-8')}")
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
 
 
 if __name__ == "__main__":
