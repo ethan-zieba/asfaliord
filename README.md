7de5e7e805b298ac1895e7ac732c22ce5c906b0f
Ethan <ethan.zieba@laplateforme.io>
2024-02-20 13:59:38 +0100

    Added ServerDatabase class + updated README.md file
    class ServerDatabase with simple CRUD operations for each table and messages flushing with sha1 hash
    README.md now has dependencies and server setup explanation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # asfaliord
 Voice/text group chat, discord inspired, software made at La Plateforme
+
+---
+**Python dependencies:** 
+
+Server and client-side: 
+`mysql, datetime, requests, http.server, random, hashlib`
+
+Server-side only:
+`mysql, datetime, uuid, http`
+
+Client-side only:
+`requests`
+---
+**Setup for the server:**
+
+1. You will need tor, and tor-browser installed
+2. Edit the torrc file to uncomment/add these two lines:  
+`HiddenServiceDir /var/lib/tor/server_directory/`
+
+    `HiddenServicePort 80 127.0.0.1:8080`
+3. Go into your `/var/lib/tor/server_directory/` and get your .onion address from the hostname file
+4. Anyone that wants to connect to your Asfaliord server will need this address
---
+++ server_database.py
@@ -0,0 +1,98 @@
+import mysql.connector
+from datetime import datetime
+from hashlib import sha1
+from random import randint
+
+
+class ServerDatabase:
+    def __init__(self):
+        self.cnx = mysql.connector.connect(
+            host="localhost",
+            user="your_username",
+            password="your_password",
+            database="asfaliord_server"
+            )
+        self.cursor = self.cnx.cursor()
+
+    # Simple CRUD operations for each table of the server-side database
+    def create_user(self, username, password, gpg_pk, perm_lvl):
+        query = "INSERT INTO users (username, password, gpg_pk, perm_lvl) VALUES (%s, %s, %s, %s)"
+        data = (username, password, gpg_pk, perm_lvl)
+        self.cursor.execute(query, data)
+        self.cnx.commit()
+
+    def read_user(self, user_id):
+        query = "SELECT * FROM users WHERE id = %s"
+        self.cursor.execute(query, (user_id,))
+        return self.cursor.fetchone()
+
+    def update_user(self, user_id, column, new_value):
+        data = (column, new_value, user_id)
+        query = "UPDATE users SET username = %s WHERE id = %s"
+        self.cursor.execute(query, data)
+        self.cnx.commit()
+
+    def delete_user(self, user_id):
+        query = "DELETE FROM users WHERE id = %s"
+        self.cursor.execute(query, (user_id,))
+        self.cnx.commit()
+
+    # Channels CRUD operations
+    def create_channel(self, name, min_perm_lvl, is_text):
+        data = (name, min_perm_lvl, is_text)
+        query = "INSERT INTO channels (name, min_perm_lvl, is_text) VALUES (%s, %s)"
+        self.cursor.execute(query, data)
+        self.cnx.commit()
+
+    def read_channel(self, channel_id):
+        query = "SELECT * FROM channels WHERE id = %s"
+        self.cursor.execute(query, (channel_id,))
+        return self.cursor.fetchone()
+
+    def update_channel(self, channel_id, column, new_value):
+        data = (column, new_value, channel_id)
+        query = "UPDATE channels SET %s = %s WHERE id = %s"
+        self.cursor.execute(query, data)
+        self.cnx.commit()
+
+    def delete_channel(self, channel_id):
+        query = "DELETE FROM channels WHERE id = %s"
+        self.cursor.execute(query, (channel_id,))
+        self.cnx.commit()
+
+    # Messages CRUD operations
+    def create_message(self, user_id, channel_id, content):
+        data = (user_id, channel_id, content, datetime.now().strftime("%Y/%M/%D %H:%M:%S"))
+        query = "INSERT INTO messages (user_id, channel_id, content, time) VALUES (%s, %s, %s, %s)"
+        self.cursor.execute(query, data)
+        self.cnx.commit()
+
+    def read_message(self, message_id):
+        query = "SELECT * FROM messages WHERE id = %s"
+        self.cursor.execute(query, (message_id,))
+        return self.cursor.fetchone()
+
+    def update_message(self, message_id, column, new_value):
+        data = (column, new_value, message_id)
+        query = "UPDATE messages SET %s = %s WHERE id = %s"
+        self.cursor.execute(query, data)
+        self.cnx.commit()
+
+    def delete_message(self, message_id):
+        query = "DELETE FROM messages WHERE id = %s"
+        self.cursor.execute(query, (message_id,))
+        self.cnx.commit()
+
+    def flush_messages(self):
+        self.cursor.execute("SELECT COUNT(*) FROM messages")
+        messages_count = self.cursor.fetchone()[0]
+        for i in range(messages_count):
+            # Creates a sha1 (fastest sha, more time-efficient relative to our needs) that replaces the hashed message
+            # content. For basic opsec purposes, making it look like a real message for the human eye.
+            data = (sha1(chr(randint(96, 122)).encode('utf-8')).hexdigest(), i)
+            query = (f"UPDATE messages SET content = %s WHERE id = %s")
+            self.cursor.execute(query, data)
+            self.cnx.commit()
+    def close_connection(self):
+        self.cursor.close()
+        self.cnx.close()
