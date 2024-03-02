ac09391211b555785ae76fd09813832814c29b31
Ethan <ethan.zieba@laplateforme.io>
2024-03-02 14:51:48 +0100

    Correction create_tables.py: better syntax

--- server/databases/create_tables.py
+++ server/databases/create_tables.py
@@ -11,27 +11,27 @@ cnx = mysql.connector.connect(
 cursor = cnx.cursor()
 
 # DATE in SQL is in the format YYYY-MM-DD
-messages_query = ("CREATE TABLE 'messages' ("
-                  "'id' INT NOT NULL AUTO_INCREMENT,"
-                  "'content' TEXT DEFAULT '',"
-                  "'channel_id' INT NOT NULL,"
-                  "'time_to_live' DATE,"
-                  "PRIMARY KEY ('id')"
+messages_query = ("CREATE TABLE messages ("
+                  "id INT NOT NULL AUTO_INCREMENT,"
+                  "content TEXT DEFAULT '',"
+                  "channel_id INT NOT NULL,"
+                  "time_to_live DATE,"
+                  "PRIMARY KEY (id)"
                   ")")
 
-channel_query = ("CREATE TABLE 'channels' ("
-                 "'id' INT NOT NULL AUTO_INCREMENT, "
-                 "'name' TEXT DEFAULT '', "
-                 "'permissions_level' INT NOT NULL DEFAULT '1', "
-                 "PRIMARY KEY (`id`)"
+channel_query = ("CREATE TABLE channels ("
+                 "id INT NOT NULL AUTO_INCREMENT, "
+                 "name TEXT DEFAULT '', "
+                 "permissions_level INT NOT NULL DEFAULT 1, "
+                 "PRIMARY KEY (id)"
                  ")")
 
-users_query = ("CREATE TABLE `channels` ("
-               "`id` INT NOT NULL AUTO_INCREMENT,"
-               "`username` VARCHAR(25) NOT NULL,"
-               "`password` VARCHAR(255) NOT NULL,"
-               "`public_gpg` VARCHAR(500),"
-               "PRIMARY KEY (`id`)"
+users_query = ("CREATE TABLE channels ("
+               "id INT NOT NULL AUTO_INCREMENT,"
+               "username VARCHAR(25) NOT NULL,"
+               "password VARCHAR(255) NOT NULL,"
+               "public_gpg VARCHAR(500),"
+               "PRIMARY KEY (id)"
                ")")
 
 cursor.execute(messages_query)
